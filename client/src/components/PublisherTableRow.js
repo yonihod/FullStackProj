bc278a098662593f2b7bd158f05c1f58b1f4b93f
yonih <yonihod199@gmail.com>
2020-02-22 21:27:42 +0200

    Added get and list on publishers

---
+++ client/src/components/PublisherTableRow.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
+export default class PublisherTableRow extends Component {
+
+    render() {
+        return (
+            <tr>
+                <td>{this.props.obj.name}</td>
+                <td>{this.props.obj.email}</td>
+                <td>{this.props.obj.rollno}</td>
+                <td>
+                    <Link className="edit-link" to={"/edit-student/" + this.props.obj._id}>
+                        Edit
+                    </Link>
+                    <Button size={"sm"} variant={"danger"}> Delete </Button>
+                </td>
+            </tr>
+        )
+    }
+}
