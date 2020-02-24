1bcae53d871619eabe1cb586b6f0dc97debfae09
Alon Zinar <alonzinerziner@cs.colman.ac.il>
2020-02-24 20:17:44 +0200

    add Google/Facebook/GitHub/Email user authentication with firebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from 'react';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
@@ -7,63 +7,71 @@ import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import CreatePublisher from "./components/create-publisher.component";
 import EditPublisher from "./components/edit-publisher.component";
 import PublisherList from "./components/list-publisher.component";
+import Login from "./components/login.component";
 
-function App() {
-    return (<Router>
-        <div className="App">
-            <header className="App-header">
-                <Navbar bg="dark" variant="dark">
-                    <Container>
-                        <Navbar.Brand>
-                            <Link to={"/create-publisher"} className="nav-link">
-                                Developi
+export default class App extends Component {
+    render() {
+        return (<Router>
+            <div className="App">
+                <header className="App-header">
+                    <Navbar bg="dark" variant="dark">
+                        <Container>
+                            <Navbar.Brand>
+                            <Link to={"/login"} className="nav-link">
+                                    Sign in
                             </Link>
-                        </Navbar.Brand>
+                            </Navbar.Brand>
 
-                        <Nav className="justify-content-end">
-                            <Nav>
+                            <Navbar.Brand>
                                 <Link to={"/create-publisher"} className="nav-link">
-                                    Create Publisher
-                                </Link>
-                            </Nav>
+                                    Developi
+                            </Link>
+                            </Navbar.Brand>
 
-                            <Nav>
-                                <Link to={"/edit-publisher/:id"} className="nav-link">
-                                    Edit Publisher
+                            <Nav className="justify-content-end">
+                                <Nav>
+                                    <Link to={"/create-publisher"} className="nav-link">
+                                        Create Publisher
                                 </Link>
-                            </Nav>
+                                </Nav>
 
-                            <Nav>
-                                <Link to={"/list-publisher"} className="nav-link">
-                                    Publisher List
+                                <Nav>
+                                    <Link to={"/edit-publisher/:id"} className="nav-link">
+                                        Edit Publisher
                                 </Link>
-                            </Nav>
-                        </Nav>
-                    </Container>
-                </Navbar>
-            </header>
+                                </Nav>
 
-            <Container>
-                <Row>
-                    <Col md={12}>
-                        <div className="wrapper">
-                            <Switch>
-                                <Route exact path='/' component={CreatePublisher}/>
-                                <Route path="/create-publisher" component={CreatePublisher}/>
-                                <Route path="/edit-publisher/:id" component={EditPublisher}/>
-                                <Route path="/list-publisher" component={PublisherList}/>
-                            </Switch>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    </Router>);
+                                <Nav>
+                                    <Link to={"/list-publisher"} className="nav-link">
+                                        Publisher List
+                                </Link>
+                                </Nav>
+                            </Nav>
+                        </Container>
+                    </Navbar>
+                </header>
+
+                <Container>
+                    <Row>
+                        <Col md={12}>
+                            <div className="wrapper">
+                                <Switch>
+                                    <Route exact path='/' component={CreatePublisher} />
+                                    <Route exact path='/login' component={Login} />
+                                    <Route path="/create-publisher" component={CreatePublisher} />
+                                    <Route path="/edit-publisher/:id" component={EditPublisher} />
+                                    <Route path="/list-publisher" component={PublisherList} />
+                                </Switch>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </Router>)
+    }
 }
-
-export default App;
---
+++ client/src/components/login.component.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDYo5i3ypxRK2XYwRRfoVo6rSjka5-9I9c",
+    authDomain: "developi.firebaseapp.com"
+})
+
+export default class Login extends Component {
+    state = { isSignedIn: false }
+    uiConfig = {
+        signInFlow: "popup",
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        callbacks: {
+            signInSuccess: () => console.log(firebase.auth().currentUser)
+        }
+    }
+
+    componentDidMount = () => {
+        firebase.auth().onAuthStateChanged(user => {
+            this.setState({ isSignedIn: !!user })
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.isSignedIn ? (
+                    <span>
+                        <img alt="profile picture" src={firebase.auth().currentUser.photoURL} />
+                        Welcome {firebase.auth().currentUser.displayName}
+                        <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+                    </span>) : (
+                        <StyledFirebaseAuth
+                            uiConfig={this.uiConfig}
+                            firebaseAuth={firebase.auth()}
+                        />
+                    )}
+            </div>)
+    }
+}
