42031d15dc25fec28699ec8afb349bdc29dffa76
yonih <yonihod199@gmail.com>
2020-02-29 14:02:41 +0200

    small fix

--- client/src/components/post/PostBox.js
+++ client/src/components/post/PostBox.js
@@ -3,10 +3,7 @@ import {Card,Button} from 'react-bootstrap'
 import {Link} from "react-router-dom";
 
 export default class PostBox extends Component {
-    constructor(props) {
-        super();
-        this.date = new Date(props.obj.createdAt);
-    }
+
     render() {
         return (
                 <Card className={"w-25 p-3"}>
@@ -15,11 +12,10 @@ export default class PostBox extends Component {
                         <Card.Title>
                             {this.props.obj.title}
                         </Card.Title>
-                        <Card.Subtitle>{this.date.toLocaleDateString('en-gb')}</Card.Subtitle>
                         <Card.Text>
                             {this.props.obj.description}
                         </Card.Text>
-                        <Link to={`posts/${this.props.obj._id}`}>
+                        <Link to={`/posts/${this.props.obj._id}`}>
                             <Button variant={"primary"}>Explore</Button>
                         </Link>
                     </Card.Body>
