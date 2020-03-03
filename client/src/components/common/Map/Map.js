1a8ebfcae9cfe6ded3f5778d5e15c421f01f4517
orow1993 <orow1993@gmail.com>
2020-03-03 13:37:36 +0200

    Addresses

--- client/src/components/common/Map/Map.js
+++ client/src/components/common/Map/Map.js
@@ -1,39 +1,61 @@
 import React, {Component} from 'react'
 import {Map, GoogleApiWrapper, Marker} from 'google-maps-react';
 import "./Map.css";
+import AddressesService from "../../../services/Addresses";
 
 class MapContainer extends Component {
+
     constructor(props) {
         super(props);
 
         this.state = {
-            stores: [{latitude: 31.970004, longitude: 34.770976}]
+            name: 'name',
+            lat: 'lat',
+            lng: 'lng'
+        };
+
+        AddressesService.getAddress(props.id).then(data => {
+            console.log(data);
+            this.setState(
+                {
+                    name: data.name,
+                    lat: data.lat,
+                    lng: data.lng
+                })
+        }).catch(err => {
+            console.log(err);
+        });
+
+        this.state = {
+            stores: [{latitude: this.state.lat, longitude: this.state.lng}]
+
         }
     }
 
+
     displayMarkers = () => {
-        return this.state.stores.map((store, index) => {
+        return this.state.stores.map((props, index) => {
             return <Marker key={index} id={index} position={{
-                lat: store.latitude,
-                lng: store.longitude
+                lat: props.latitude,
+                lng: props.longitude
             }}
                            onClick={() => console.log("You clicked me!")}/>
         })
     };
 
-    render() {
+    render(){
         return (
             <Map className="MapStyle"
-                google={this.props.google}
-                zoom={8}
-                initialCenter={{lat: 31.970004, lng: 34.770976}}
+                 google={this.props.google}
+                 zoom={8}
+                 initialCenter={{lat: this.state.latitude, lng: this.state.longitude}}
             >
                 {this.displayMarkers()}
             </Map>
         );
     }
 }
-
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyANXoJgfGOa7RzybrEblW5oi9Tcze7MVnU'
 })(MapContainer);
+
---
+++ server/routes/addresses.js
@@ -0,0 +1,45 @@
+const Address = require('../models/address');
+
+module.exports = (app) => {
+    app.route('/addresses')
+        .get((req, res) => {
+            Address.find().populate('skills').then((data) => {
+                res.status(200).json(data);
+            }).catch((err) => {
+                console.log(err);
+            });
+        })
+        .post((req, res) => {
+            Address.create(req.body).then((data) => {
+                res.status(201).json(data);
+            }).catch(err => {
+                console.log(err);
+            });
+        });
+
+    app.route('/address/:id')
+        .get((req, res) => {
+            Address.findById(req.params.id).populate('skills posts').then((data) => {
+                res.status(200).json(data);
+            }).catch((err) => {
+                console.log(err);
+            });
+        })
+        .put((req, res) => {
+            Address.findByIdAndUpdate(req.params.id, req.body, {
+                new: true,
+                upsert: true
+            }).then(data => {
+                res.status(200).json(data);
+            }).catch(err => {
+                console.log(err);
+            });
+        })
+        .delete((req, res) => {
+            Address.findByIdAndRemove(req.params.id).then(data => {
+                res.status(200).json(data);
+            }).catch(err => {
+                console.log(err);
+            });
+        })
+};
