55e334ca44642dc88f817c3b8753ce69a1d171bc
orow1993 <orow1993@gmail.com>
2020-03-04 10:43:08 +0200

    Map +css

--- client/src/components/common/Map/Map.js
+++ client/src/components/common/Map/Map.js
@@ -1,24 +1,41 @@
-import React, {Component} from 'react'
-import {Map, GoogleApiWrapper, Marker} from 'google-maps-react';
+import React, { Component } from 'react'
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+
 import "./Map.css";
+import AddressesService from "../../../services/Addresses";
+import PostsService from "../../../services/Posts";
+import PostBox from "../../post/PostBox";
 
 class MapContainer extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            stores: [{latitude: 31.970004, longitude: 34.770976}]
-        }
+            addresses: []
+        };
+    }
+
+    componentDidMount() {
+        AddressesService.getAddresses().then(data => {
+            this.setState({ addresses: data })
+        }).catch(err => {
+            console.log(err);
+        });
     }
 
     displayMarkers = () => {
-        return this.state.stores.map((store, index) => {
-            return <Marker key={index} id={index} position={{
-                lat: store.latitude,
-                lng: store.longitude
-            }}
-                           onClick={() => console.log("You clicked me!")}/>
-        })
+        if (this.state.addresses) {
+            return this.state.addresses.map((data, index) => {
+                if (data !== undefined) {
+                    return <Marker key={index} id={index} position={{
+                        lat: this.state.addresses[index].lat,
+                        lng: this.state.addresses[index].lng
+                    }}
+                    onClick={() => console.log(this.state.addresses[index].name)} />
+                }
+            })
+        }
     };
 
     render() {
@@ -26,14 +43,13 @@ class MapContainer extends Component {
             <Map className="MapStyle"
                 google={this.props.google}
                 zoom={8}
-                initialCenter={{lat: 31.970004, lng: 34.770976}}
-            >
+                initialCenter={{ lat: 31.970004, lng: 34.770976 }}>
                 {this.displayMarkers()}
             </Map>
         );
     }
 }
-
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyANXoJgfGOa7RzybrEblW5oi9Tcze7MVnU'
 })(MapContainer);
+
