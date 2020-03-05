b505e5fd6b4dd9683f7e5f3810680089eacec5df
yonih <yonihod199@gmail.com>
2020-03-05 19:34:46 +0200

    security fix

--- server/routes/twitter.js
+++ server/routes/twitter.js
@@ -1,13 +1,6 @@
 var Twit = require('twit');
-
-var T = new Twit({
-    consumer_key:         'a2MbBIHUN4BvlgTRgIJRw6rj2',
-    consumer_secret:      'YLhW7Y5pX7LjfNaOOsM8J1w1Jl3qfAccoRHHuCeNRBkj2IXtCE',
-    access_token:         '1235389049264893959-rKzhUthPLLzS5aHYLIXqeu9nCf7faB',
-    access_token_secret:  'ZtdGtlyDSEDjnQJrwDjvBqq41X8sixdJSncUPOn8JN6hd',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL:            true,     // optional - requires SSL certificates to be valid.
-});
+var config = require('../config');
+var T = new Twit(config);
 
 module.exports = (app) => {
     app.route('/twitter')
