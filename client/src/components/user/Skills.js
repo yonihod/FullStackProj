baacfd52e389e4dab7e2fef6ae56181c0370edea
KamilBecken <kamilbecken@gmail.com>
2020-03-05 18:27:04 +0200

    add user not found message and skills component

---
+++ client/src/components/user/Skills.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import UserService from "../../services/Users";
+
+
+export default class Skills extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            skills: []
+        };
+    }
+
+    componentDidMount() {
+        UserService.getUser(this.props.match.params.id).then(res => {
+            this.setState({
+                skills: JSON.parse(res).skills
+            });
+        }).catch(err => {
+            console.log('There has been an error loading skills in skills-component: ' + err);
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.state.skills.map(skill => {
+                        return <li>{skill[0]}</li>
+                    })}
+                </ul>
+            </div>
+        );
+    }
+
+    }
