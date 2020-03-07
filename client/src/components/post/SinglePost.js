692afdfc1c7705ea9f8c07db4c6354d6241aadb1
yonih <yonihod199@gmail.com>
2020-03-07 16:56:17 +0200

    Added spinner while waiting for response from twitter + some server perks

--- client/src/components/post/SinglePost.js
+++ client/src/components/post/SinglePost.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PostsService from "../../services/Posts";
 import TwitterService from "../../services/Twitter"
-import Button from 'react-bootstrap/Button';
+import {Button, Spinner} from 'react-bootstrap';
 
 export default class SinglePost extends Component {
     constructor(props) {
@@ -13,7 +13,8 @@ export default class SinglePost extends Component {
             desc: 'My Post desc',
             owner: 'My Post owner',
             dueDate: 'My Post Date',
-            tags: ['My Tags', 'Second Tag']
+            tags: ['My Tags', 'Second Tag'],
+            disableTwitterButton: false
         };
 
         PostsService.getPost(props.match.params.id).then(data => {
@@ -30,21 +31,53 @@ export default class SinglePost extends Component {
             console.log(err);
         });
     }
+
     publish = () => {
-        TwitterService.postTwit({title:this.state.title}).then((res)=>{
-           console.log(res);
+        this.setState({disableTwitterButton: true});
+        TwitterService.postTwit({title: this.state.title}).then((res) => {
+            console.log(res);
+            this.setState({disableTwitterButton: false});
         });
+    };
+    spinner = () => {
+        if (this.state.disableTwitterButton) {
+            return (
+                <span>
+                    <Spinner
+                        as="span"
+                        animation="border"
+                        size="sm"
+                        role="status"
+                        aria-hidden="true"
+                        className={"mr-2"}
+                    />
+                    Posting...
+                </span>
+            );
+        }
+    };
+    postToTwitter = () => {
+        if(!this.state.disableTwitterButton) {
+            return (
+                <span>
+                Post To Twitter
+                <span className={'ml-2'}>
+                    <i className={"fab fa-twitter"}/>
+                </span>
+            </span>
+            );
+        }
     };
 
     render() {
         return (
             <div>
-                <Button className={'m-2'} onClick={this.publish}>
-                    <span className={'mr-2'}>
-                        Post To Twitter
-                    </span>
-                    <i className={"fab fa-twitter"}></i>
-                </Button>
+                <div>
+                    <Button className={'m-2'} onClick={this.publish} disabled={this.state.disableTwitterButton}>
+                        {this.postToTwitter()}
+                        {this.spinner()}
+                    </Button>
+                </div>
                 <h1>{this.state.title}</h1>
                 <div>
                     <h4>{this.state.owner} / {this.state.dueDate}</h4>
@@ -52,10 +85,7 @@ export default class SinglePost extends Component {
                     <h3>Tags:</h3>
                     {this.state.tags.map(t => <li key={t}>{t}</li>)}
                 </div>
-
             </div>
-
-
         );
     }
 }
