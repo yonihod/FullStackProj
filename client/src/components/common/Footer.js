79c5f7dbdff6b7987ed19c5c94b316d894ef8b07
Chen Eini <chen.eini000@gmail.com>
2020-03-07 02:55:02 +0200

    fix footer position

--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
 
     render() {
         return (
-            <footer className={"footer bg-dark w-100 p-4"}>
+            <footer className={"footer bg-dark w-100"}>
                 © Developi
             </footer>
         );
