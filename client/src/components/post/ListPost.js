361a76cb11686e760b15d805a37c32513eb75c40
orow1993 <orow1993@gmail.com>
2020-03-09 00:22:37 +0200

    Css posts+about

--- client/src/components/post/ListPost.js
+++ client/src/components/post/ListPost.js
@@ -96,7 +96,8 @@ export default class ListPost extends Component {
         return (
             <div className={"post-page"}>
                 {this.state.done ? <Success value={this.state}/> : null}
-                <div className="Writer mt-5">
+                <img src="/posts-people.jpg" className="posts_img"/>
+                <div className="writer mt-5">
                     <Typist>
                         <h1>Choose your task</h1> <br/>
                         <h4>A single place, millions of creative talents<br/>Improve your quality of life with style
