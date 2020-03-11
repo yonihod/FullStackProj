c6ecb76caae3aa551bc60db2f133b0b70337f2fb
chenEini <chen.eini000@gmail.com>
2020-03-11 19:59:22 +0200

    use the stackoverflow web service

--- server/lib/mongoose.js
+++ server/lib/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const {fillDbWithPosts} = require('../services/stackoverflow');
 
 module.exports.connect = () => {
     return mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true})
         .then(() => {
             console.log('Connected to mongo db');
+            fillDbWithPosts();
         });
 };
