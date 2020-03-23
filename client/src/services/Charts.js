a7e46110828364de3c525ccbdd5da8e175230e42
Alon Ziner <AlonZinerZiner@gmail.com>
2020-03-23 14:11:19 +0200

    remove graphs demo data and move data manipulation to the server side

--- client/src/services/Charts.js
+++ client/src/services/Charts.js
@@ -1,67 +1,23 @@
-import PostsService from "../services/Posts";
-import UsersService from "../services/Users";
+import axios from 'axios';
 
 export default class ChartsService {
+    static CHARTS_API = `${process.env.REACT_APP_API_URL}`;
+
     static getMostPopularPosts() {
-        return PostsService.getPosts().then(res => {
-            // get only posts with tags
-            const d = res.filter(p => p.hasOwnProperty('tags') && p.tags.length > 0);
-
-            // create object with all the tags and their count
-            const tagsWithCount = d.flatMap(p => p.tags).reduce((a, c) =>
-            (a[c] = (a[c] || 0) + 1, a), Object.create(null));
-            
-            // map the object to array
-            var array = Object.keys(tagsWithCount).map(function (key) {
-                return [(key), tagsWithCount[key]];
+        return axios.get(`${this.CHARTS_API}/commonTags`)
+            .then((response) => {
+                return response.data;
+            }).catch((error) => {
+                console.log(error)
             });
-
-            // sort and take first 10
-            const sorted = array.sort((x, y) => y[1] - x[1]).slice(0, 10)
-
-            // map to match the chart data template
-            const mapped = sorted.map(function (x) {
-                return {
-                    x: x[0],
-                    y: x[1]
-                };
-            });
-            
-            // final data
-            const data = {
-                key: 'Most Common Posts Tags', values: mapped
-            };
-
-            return data;
-        }).catch(err => {
-            console.log(err)
-        });
     }
 
-    static getUsersSkills() {
-        UsersService.getUsers().then(res => {
-            const usersWithSkills = res.filter(p => p.hasOwnProperty('skills') && p.skills.length > 0);
-            const tagsWithCount = usersWithSkills.flatMap(p => p.skills).reduce((a, c) => (a[c] = (a[c] || 0) + 1, a), Object.create(null));
-            var array = Object.keys(tagsWithCount).map(function (key) {
-                return [(key), tagsWithCount[key]];
+    static getMostActiveUsers() {
+        return axios.get(`${this.CHARTS_API}/activeUsersPosts`)
+            .then((response) => {
+                return response;
+            }).catch((error) => {
+                console.log(error)
             });
-            const sorted = array.sort((x, y) => y[1] - x[1]).slice(0, 10)
-
-            // map to match the chart data template
-            const mapped = sorted.map(function (x) {
-                return {
-                    x: x[0],
-                    y: x[1]
-                };
-            });
-            
-            // final data
-            const data = {
-                key: 'Users Skills', values: mapped
-            };
-
-        }).catch(err => {
-            console.log(err)
-        });
     }
 }
---
+++ server/routes/charts.js
@@ -0,0 +1,97 @@
+const Post = require('../models/post');
+
+module.exports = (app) => {
+    app.route('/commonTags')
+        .get((req, res) => {
+            Post.find({})
+                .then(posts => {
+                    // filter posts with no tags and create object with all the tags and their count
+                    const tagsWithCount = posts.filter(p => p.tags.length > 0).flatMap(p => p.tags)
+                        .reduce((a, c) => (a[c] = (a[c] || 0) + 1, a), Object.create(null));
+
+                    // map the object to array
+                    var array = Object.keys(tagsWithCount).map(function (key) {
+                        return [(key), tagsWithCount[key]];
+                    });
+
+                    // sort and take first 10
+                    const sorted = array.sort((x, y) => y[1] - x[1]).slice(0, 10)
+
+                    // map to match the chart data template
+                    const mapped = sorted.map(function (x) {
+                        return {
+                            x: x[0],
+                            y: x[1]
+                        };
+                    });
+
+                    // final data
+                    const data = {
+                        key: 'Most Common Posts Tags', values: mapped
+                    };
+
+                    res.status(201).json(data);
+                }).catch((err) => {
+                    console.log(err);
+                });
+        });
+
+    app.route('/activeUsersPosts')
+        .get((req, res) => {
+            const data = Post.aggregate([
+                {
+                    // make sure that the post has owner property
+                    $match: {
+                        "owner": {
+                            "$exists": true,
+                            "$ne": null
+                        },
+                        "owner.name": {
+                            "$ne": 'System'
+                        }
+                    }
+                },
+                {
+                    // populate owner from users
+                    $lookup:
+                    {
+                        from: 'users',
+                        localField: 'owner',
+                        foreignField: '_id',
+                        as: 'owner'
+                    }
+                },
+                {
+                    $unwind: '$owner'
+                },
+                {
+                    // group the post by user
+                    $group: {
+                        _id: { 'owner': "$owner" },
+                        count: { $sum: 1 }
+                    }
+                },
+                {
+                    // sort in descending order
+                    $sort: { count: -1 }
+                },
+                {
+                    // take first 6 (5 + System user)
+                    $limit: 6
+                }
+            ]
+            ).exec().then(users => {
+                const data = users.filter(user => user._id.owner.name !== 'System')
+                .map(function (user) {
+                    return {
+                        label: user._id.owner.name,
+                        value: user.count
+                    }
+                });
+
+                res.status(201).json(data);
+            }).catch((err) => {
+                console.log(err);
+            });
+        });
+};
