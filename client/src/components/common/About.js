604bc717c675ffcf7cf11463b97f8ba11f12d1a1
Chen Eini <chen.eini000@gmail.com>
2020-05-02 21:12:19 +0300

    fixes in about us page fixes

--- client/src/components/common/About.js
+++ client/src/components/common/About.js
@@ -1,40 +1,33 @@
 import React, {Component} from 'react'
 import Map from "./map/Map";
 import Charts from './charts/Charts';
-import Canvas from "./privateRoute/Canvas";
 
 export default class about extends Component {
 
-    componentDidMount(): void {
+    componentDidMount() {
         let c = document.getElementById("myCanvas");
         let ctx = c.getContext("2d");
-
-
         ctx.font = "20px Verdana";
-// Create gradient
+        // Create gradient
         let gradient = ctx.createLinearGradient(0, 0, c.width, 0);
-        gradient.addColorStop("0"," magenta");
+        gradient.addColorStop("0", " magenta");
         gradient.addColorStop("0.5", "blue");
         gradient.addColorStop("1.0", "red");
-// Fill with gradient
+        // Fill with gradient
         ctx.fillStyle = gradient;
         ctx.fillText("Thank You For Your Interest!", 10, 90);
-
     }
 
     render() {
         return (
             <div className={"about"}>
                 <section className="head">
-
+                    <img src={`${process.env.PUBLIC_URL}/about-us.jpg`}/>
                     <h1>Welcome To DEVELOPI!</h1>
                     <p className="body_text">
                         Our product is a combination of existing solutions,<br/>
                         focusing on one niche so it would be more<br/>
                         suitable for developers and answer their needs.
-                        <br/>
-                        The existing solutions that correspond with our product are mainly :<br/>
-                        Fiverr and Stackoverflow.
                     </p>
                 </section>
                 <div id="our-team">
@@ -63,12 +56,11 @@ export default class about extends Component {
                     </div>
                 </div>
                 <section>
-                    <h3 className="Map_title">Visit Our Stores!</h3>
+                    <h3 className="map-title">Visit Our Stores!</h3>
                     <Map/>
                 </section>
-
                 <div className={'charts'}>
-                    <Charts />
+                    <Charts/>
                 </div>
                 <canvas id="myCanvas"/>
             </div>
