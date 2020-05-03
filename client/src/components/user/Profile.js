8b31cf9f3017d66c02178142fc493164cccfce1c
Alon Ziner <AlonZinerZiner@gmail.com>
2020-05-03 23:38:42 +0300

    add skills to user

--- client/src/components/user/Profile.js
+++ client/src/components/user/Profile.js
@@ -1,30 +1,35 @@
-import React, {useEffect, useState} from "react";
-import {useAuth0} from "../../reactAuth0";
+import React, { useEffect, useState } from "react";
+import { useAuth0 } from "../../reactAuth0";
 import UserService from "../../services/Users";
 import PostBox from "../post/PostBox";
-import {Badge} from "react-bootstrap";
+import { Badge } from "react-bootstrap";
 import Skills from "../user/Skills";
 import Spinner from "react-bootstrap/Spinner";
 
 const Profile = () => {
 
     const [userFromDB, setUserFromDB] = useState({});
-    const {loading, user} = useAuth0();
+    const [skills, setSkills] = useState([]);
+    const { loading, user } = useAuth0();
 
     useEffect(() => {
-        UserService.getUserByEmail(user.email).then(res => {
-            setUserFromDB(res);
-            return res;
-        }).catch(err => {
-            console.log(err);
-            return [];
-        });
+        if (!userFromDB) {
+            UserService.getUserByEmail(user.email).then(res => {
+                setUserFromDB(res);
+                setSkills(res.skills);
+                return res;
+            }).catch(err => {
+                console.log(err);
+                return [];
+            });
+        }
+
     }, [userFromDB.id]);
 
     if (loading || !userFromDB) {
         return (
             <div className={"spinner"}>
-                <Spinner animation="border" variant="primary"/>
+                <Spinner animation="border" variant="primary" />
             </div>
         );
     }
@@ -32,8 +37,19 @@ const Profile = () => {
     const dataBox = () => {
         if (userFromDB.posts?.length) {
             return userFromDB.posts.map((post, index) => {
-                return <PostBox obj={post} key={index} classList={"w-30 m-2"}/>;
+                return <PostBox obj={post} key={index} classList={"w-30 m-2"} />;
             });
+        }
+    };
+
+    const updateSkills = (newSkill) => {
+        let skill = { name: newSkill };
+        if (skills) {
+            var joined = skills.concat(skill);
+            setSkills(joined);
+        }
+        else {
+            setSkills([skill]);
         }
     };
 
@@ -44,22 +60,22 @@ const Profile = () => {
             </header>
             <div className="flex-container">
                 <div className="profile-image">
-                    <img src={user.picture} alt="Profile"/>
+                    <img src={user.picture} alt="Profile" />
                 </div>
                 <div id="user-details">
                     <div>
                         <h2>{userFromDB.name}</h2>
                         <h4>{user.email}</h4>
-                        {userFromDB.skills?.length > 0 &&
-                        (<div id="tags">
-                            {userFromDB.skills.map(t => <Badge className="mr-1 badge" key={t.name}>{t.name}</Badge>)}
-                        </div>)}
-                        {userFromDB.skills?.length === 0 &&
-                        (<div>No skills added yet</div>)}
+                        {skills?.length > 0 &&
+                            (<div id="tags">
+                                {skills.map(t => <Badge className="mr-1 badge" key={t.name}>{t.name}</Badge>)}
+                            </div>)}
+                        {skills?.length === 0 &&
+                            (<div>No skills added yet</div>)}
                     </div>
                     <div id="add-skills">
                         <h5>Add Skills</h5>
-                        <Skills/>
+                        <Skills updateSkills={updateSkills} />
                     </div>
                 </div>
                 <div className="user-posts">
