faa759b15a16aa0a10a600972f856337944def08
Chen Eini <chen.eini000@gmail.com>
2020-05-19 00:48:47 +0300

    create a service for room model

---
+++ client/src/services/Rooms.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+export default class RoomsService {
+    static ROOMS_API = `${process.env.REACT_APP_API_URL}/rooms`;
+
+    // get all the rooms and messages that belongs to the current user
+    static getCurrentUserRooms(userId) {
+        return axios.get(`${this.ROOMS_API}/${userId}`, userId)
+            .then((response) => {
+                return response.data;
+            }).catch((error) => {
+                console.log(error)
+            });
+    }
+
+    // get a specific room with messages
+    static getRoom(roomId) {
+        return axios.get(`${this.ROOMS_API}/${roomId}`, roomId)
+            .then((response) => {
+                return response.data;
+            }).catch((error) => {
+                console.log(error)
+            });
+    }
+
+    static AddRoom(room) {
+        return axios.post(this.ROOMS_API, room)
+            .then((response) => {
+                return response.data;
+            }).catch((error) => {
+                console.log(error)
+            });
+    }
+
+    static DeleteRoom(roomId) {
+        return axios.delete(`${this.ROOMS_API}/${roomId}`, roomId)
+            .then((response) => {
+                return response.data;
+            }).catch((error) => {
+                console.log(error)
+            });
+    }
+}
