cfba2c3f54765bd41faa162d0fd532d3f3179484
Chen Eini <chen.eini000@gmail.com>
2020-05-24 22:00:47 +0300

    set new error for 404

--- server/lib/express.js
+++ server/lib/express.js
@@ -28,7 +28,7 @@ function initServerRoutes(app) {
 function initErrorHandling(app) {
     // catch 404 and forward to error handler
     app.use(function (req, res, next) {
-        next(createError(404));
+        next(new Error('route not found'));
     });
 
     // error handler
