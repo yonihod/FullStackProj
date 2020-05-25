6feceb5c10beba21e327f35404cd8b0662d2a171
Chen Eini <chen.eini000@gmail.com>
2020-05-25 11:23:05 +0300

    handle 500 error

--- server/lib/express.js
+++ server/lib/express.js
@@ -32,14 +32,13 @@ function initErrorHandling(app) {
     });
 
     // error handler
-    app.use(function (err, req, res, next) {
+    app.use((err, req, res, next) => {
         // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
+        const error = req.app.get('env') === 'development' ? err : {};
 
         // render the error page
         res.status(err.status || 500);
-        res.render('error');
+        res.json(error);
     });
 }
 
